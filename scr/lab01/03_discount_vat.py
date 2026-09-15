4a22fcffab2a8c879bb4b46e5fa341a0d1f73d9e
Ageeva Anna <ageevaanna@MacBook-Air-Ageeva.local>
2026-09-15 11:20:51 +0300

    лб1

--- scr/lab01/03_discount_vat.py
+++ scr/lab01/03_discount_vat.py
@@ -4,6 +4,7 @@ vat = float(input("vat (%): "))
 
 base = price * ( 1 - discount/100)
 vat_amount = base * (vat / 100)
+
 total = base + vat_amount
 print(f"База после скидки: {base:.2f} ₽")
 print(f"НДС: {vat_amount:.2f} ₽")
